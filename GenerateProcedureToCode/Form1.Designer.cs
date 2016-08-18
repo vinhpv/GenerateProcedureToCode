4dc2247b28aa71978098fa4c8d1a4db6b359938a
Phạm Văn Vinh <vinhpv@outlook.com>
2016-08-19 00:45:19 +0700

    add new Project test

---
+++ GenerateProcedureToCode/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace GenerateProcedureToCode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtInput = new System.Windows.Forms.RichTextBox();
+            this.txtOutput = new System.Windows.Forms.RichTextBox();
+            this.txtProc = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtType = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 25);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.groupBox2);
+            this.splitContainer1.Size = new System.Drawing.Size(757, 518);
+            this.splitContainer1.SplitterDistance = 199;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(757, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(48, 22);
+            this.toolStripButton1.Text = "Run";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtInput);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(757, 199);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Input";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.txtType);
+            this.groupBox2.Controls.Add(this.txtName);
+            this.groupBox2.Controls.Add(this.txtProc);
+            this.groupBox2.Controls.Add(this.txtOutput);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(0, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(757, 315);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Output";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtInput.Location = new System.Drawing.Point(3, 16);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(751, 180);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.Text = "";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutput.Location = new System.Drawing.Point(3, 43);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(754, 271);
+            this.txtOutput.TabIndex = 1;
+            this.txtOutput.Text = "";
+            // 
+            // txtProc
+            // 
+            this.txtProc.Location = new System.Drawing.Point(77, 17);
+            this.txtProc.Name = "txtProc";
+            this.txtProc.Size = new System.Drawing.Size(118, 20);
+            this.txtProc.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Proc/Func";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(264, 17);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(183, 20);
+            this.txtName.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(223, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Name";
+            // 
+            // txtType
+            // 
+            this.txtType.Location = new System.Drawing.Point(534, 17);
+            this.txtType.Name = "txtType";
+            this.txtType.Size = new System.Drawing.Size(183, 20);
+            this.txtType.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(466, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Return type";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(757, 543);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox txtInput;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox txtOutput;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtType;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtProc;
+    }
+}
+
---
+++ GenerateProcedureToCode/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GenerateProcedureToCode
+{
+    public partial class Form1 : Form
+    {
+        bool isFunc = false;
+        string type = "DataTable", name = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            string s = txtInput.Text;
+            // chuẩn hóa loại dấu cách trắng và dấu xuống dòng
+            s = Regex.Replace(s, @"\s+", " ", RegexOptions.Multiline).ToLower().Trim();
+            var ss = s.Split(' ');
+            if (ss.Count() < 2)
+            {
+                MessageBox.Show("Chuỗi không đúng định dạng, bạn kiểm tra lại"); return;
+            }
+            // Tìm xem là function hay proc
+            txtProc.Text = ss.First();
+            // tên proc
+            txtName.Text = name = ss[1];
+            // Tìm type return
+            if (txtProc.Text == "function")
+            {
+                isFunc = true;
+                type = Parameter.getTypeNET(ss.Last());
+            }
+            // Tìm các parametor
+            List<Parameter> pras = new List<Parameter>();
+            var math = Regex.Matches(s, @"\((.+?)\)");
+            string param = "";
+            if (math.Count > 0)
+                param = math[0].Value.Replace("(", "").Replace(")", "");
+            param.Split(',').ToList().ForEach((p) =>
+            {
+                var pp = p.Trim().Split(' ');
+                pras.Add(new Parameter() { paraName = pp.First(), paraOraceType = pp.Last() });
+            });
+            if (!isFunc && pras.Where(p => p.paraOraceType.Contains("cursor")).Count() == 0)
+                type = "void";
+            txtType.Text = type;
+
+            //ghép kết quả thành chuỗi code
+            string rs = @"public " + type + " " + name + "(";
+            string thamso = "", thamsoOra = "";
+            if (isFunc)
+            {
+                thamsoOra = @"
+                new OracleParameter() { ParameterName = ""return_value"", OracleDbType = OracleDbType." + Parameter.getOracleDBType(type) + @",Direction = ParameterDirection.ReturnValue, Size = 4000},";
+            }
+            pras.ForEach((p) =>
+            {
+                thamso += p.paraType + " " + p.paraName + ",";
+                if (p.paraOraceType.Contains("cursor"))
+                {
+                    thamsoOra += @"
+                        new OracleParameter(""" + p.paraName + @""",OracleDbType.RefCursor,ParameterDirection.Output),
+                    ";
+                }
+                else
+                {
+                    thamsoOra += @"
+                               new OracleParameter() {ParameterName = """ + p.paraName + @""",OracleDbType = OracleDbType." + p.oraceType + @", Value = " + p.paraName + @"},";
+                }
+            });
+            rs += thamso.Remove(thamso.Length - 1) + " )";// loại dấu , cuối
+            rs += @"
+            {
+                string str = """";
+                str = DatabaseConstants.DB2 + ""." + name + @""";
+                OracleParameter[] parms;
+                parms = new OracleParameter[]
+                                {" + thamsoOra + @"
+                };";
+            if (type == "void")
+            {
+                rs += @"
+                    OracleDataAccess.ExecuteNonQuery(CommandType.StoredProcedure, str, parms);
+         }
+                ";
+            }
+            else if (type == "DataTable")
+            {
+                rs += @"
+                    return OracleDataAccess.getDataFromProcedure(str, ""table"", parms).Tables[0];
+         }
+                ";
+            }
+            else
+            {
+                rs += @"
+                    OracleDataAccess.ExecuteNonQuery(CommandType.StoredProcedure, str, parms);
+                    Return parms[0].Value as " + type + @"
+         }
+                ";
+            }
+            /*
+                    public void thaydoi_ttkn(long vkhieunai_id, int vnhanvien_id, int vdonvi_id, int vtrangthai_id,
+            string vghichu, string vdienthoai_lh, string vnd_khieunai, string vnoidung_gq, string vnguyennhan_kn,
+            decimal vgiamcuoc_dt, decimal vvat_dt, decimal vgiamcuoc_cp, decimal vvat_cp, DateTime vngay_cn,
+            string vnguoi_cn, string vmay_cn, string vip_cn
+            )
+        {
+            string str = "";
+            str = DatabaseConstants.DB3 + ".thaydoi_ttkn";
+            OracleParameter[] parms;
+            parms = new OracleParameter[] 
+							{
+                                new OracleParameter("vkhieunai_id", OracleDbType.Int64),
+                                new OracleParameter("vngay_gq", OracleDbType.Date),
+                                new OracleParameter("vnhanvien_gq_id", OracleDbType.Int32),
+                                new OracleParameter("vnoidung_gq", OracleDbType.Varchar2),
+                                new OracleParameter("vnd_ton", OracleDbType.Varchar2),
+                                new OracleParameter("vnguyennhan", OracleDbType.Varchar2),
+                                new OracleParameter("vtien_gc", OracleDbType.Decimal),
+                                new OracleParameter("vvat_gc", OracleDbType.Decimal),
+			};
+            parms[0].Value = vkhieunai_id;
+            parms[1].Value = vngay_gq;
+            parms[2].Value = vnhanvien_gq_id;
+            parms[3].Value = vnoidung_gq;
+            parms[4].Value = vnd_ton;
+            parms[5].Value = vnguyennhan;
+            parms[6].Value = vtien_gc;
+            parms[7].Value = vvat_gc;
+            OracleDataAccess.ExecuteNonQuery(CommandType.StoredProcedure, str, parms);
+        }
+            */
+            txtOutput.Text = rs;
+        }
+
+        internal class Parameter
+        {
+            public string paraName { get; set; }
+            public string paraOraceType { get; set; }
+            public string parameterDirection { get; set; }
+            public string paraType
+            {
+                get
+                {
+                    return getTypeNET(paraOraceType);
+                }
+            }
+            public string oraceType
+            {
+                get
+                {
+                    return getOracleType(paraOraceType);
+                }
+            }
+            public static string getTypeNET(string OraceType)
+            {
+                string s = "";
+                switch (OraceType)
+                {
+                    case "number":
+                        s = "decimal";
+                        break;
+                    case "date":
+                        s = "DateTime";
+                        break;
+                    default:
+                        s = "string";
+                        break;
+                }
+                return s;
+            }
+            public static string getOracleType(string paraOraceType)
+            {
+                string s = "";
+                switch (paraOraceType)
+                {
+                    case "number":
+                        s = "Decimal";
+                        break;
+                    case "date":
+                        s = "Date";
+                        break;
+                    default:
+                        s = "Varchar2";
+                        break;
+                }
+                return s;
+            }
+            public static string getOracleDBType(string type)
+            {
+                string s = "";
+                switch (type)
+                {
+                    case "decimal":
+                        s = "Decimal";
+                        break;
+                    case "DataTime":
+                        s = "Date";
+                        break;
+                    default:
+                        s = "Varchar2";
+                        break;
+                }
+                return s;
+            }
+        }
+    }
+}
